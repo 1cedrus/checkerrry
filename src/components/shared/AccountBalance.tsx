716c470f5cbb73a77550c4ad56e73cf0ea876ae7
shouse <aowiej23s@gmail.com>
2023-07-29 13:02:12 +0700

    refactor: optimize code

--- src/components/shared/AccountBalance.tsx
+++ src/components/shared/AccountBalance.tsx
@@ -27,6 +27,12 @@ export default function AccountBalance() {
   const { address } = useAddressContext();
   const [balances, setBalances] = useState<Object>({});
 
+  if (!address) return;
+
+  if (!isValidAddressPolkadotAddress(address)) {
+    return <Text>Error occurred (Invalid address)</Text>;
+  }
+
   useAsync(async () => {
     if (!address || !apis) return;
 
@@ -39,12 +45,6 @@ export default function AccountBalance() {
       });
     });
   }, [address, apis]);
-
-  if (!address) return;
-
-  if (!isValidAddressPolkadotAddress(address)) {
-    return <Text>Error occurred (Invalid address)</Text>;
-  }
 
   return (
     <Box>
