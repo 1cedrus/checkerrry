35f0546d30f8757cba750c149f009533ff872a14
shouse <aowiej23s@gmail.com>
2023-08-09 10:05:36 +0700

    refactor: optimize code

--- src/components/layouts/NavigateButton.tsx
+++ src/components/layouts/NavigateButton.tsx
@@ -1,12 +1,32 @@
 import { Box, Button, SlideFade, useTab, useDisclosure, Flex } from '@chakra-ui/react';
 import { Tabs, TabList } from '@chakra-ui/react';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { SettingsIcon } from '@chakra-ui/icons';
+
+enum Location {
+  Welcome = '/',
+  Balances = '/balances',
+  Metadata = '/metadata',
+}
+
+const defaultIndex = (pathname: string) => {
+  switch (pathname) {
+    case Location.Welcome:
+      return 0;
+    case Location.Balances:
+      return 1;
+    case Location.Metadata:
+      return 2;
+  }
+};
 
 export default function NavigateButton() {
   const navigate = useNavigate();
   const { isOpen, onToggle } = useDisclosure();
+  const { pathname } = useLocation();
+
+  console.log(location);
 
   const CustomTab = React.forwardRef((props: any, ref: any) => {
     const tabProps = useTab({ ...props, ref });
@@ -25,10 +45,11 @@ export default function NavigateButton() {
   return (
     <Flex position='fixed' right='0'>
       <SlideFade in={isOpen} offsetY='-10rem'>
-        <Tabs variant='enclosed'>
+        <Tabs variant='enclosed' defaultIndex={defaultIndex(pathname)}>
           <TabList>
-            <CustomTab onClick={() => navigate('/balances')}>Balances</CustomTab>
-            <CustomTab onClick={() => navigate('/metadata')}>Metadata</CustomTab>
+            <CustomTab onClick={() => navigate(Location.Welcome)}>Welcome</CustomTab>
+            <CustomTab onClick={() => navigate(Location.Balances)}>Balances</CustomTab>
+            <CustomTab onClick={() => navigate(Location.Metadata)}>Metadata</CustomTab>
           </TabList>
         </Tabs>
       </SlideFade>
@@ -38,3 +59,5 @@ export default function NavigateButton() {
     </Flex>
   );
 }
+
+// TODO: handle issue about tab.
